5f5806e2d93e5a2470066101ea9b7c1f5943d3cd
Felipe Fiorini <felipefiorini90@gmail.com>
2024-04-04 09:05:53 -0300

    first commit

---
+++ ExercicioModulo4/src/br/com/ffiorini/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package br.com.ffiorini;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.print("HelloWorld");
+	}
+
+}
---
+++ ExercicioModulo4/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ExercicioModulo4 {
+}
